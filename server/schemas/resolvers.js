ac2e5121447866ff9439f01662dfb6cadfad4f17
ElmerPC <erey2790@gmail.com>
2022-04-24 17:15:20 -0500

    created the login mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,46 @@
+const { AuthenticationError } = require('apollo-server-express');
 const { Book, User } = require('../models')
+const { signToken } = require('../utils/auth')
 
 const resolvers = {
     Query: {
-       
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id }).select(
+                    "-__v -password"
+                );
+                return userData;
+            }
+            throw new AuthenticationError('Not logged in!');
+        },
+    },
+
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            // look up the user by the provided email addy. since the email field is unique, we know that only one person will exist with that email
+            const user = await User.findOne({ email });
+
+            // if there is no user with that email addy, return an auth error
+            if (!user) {
+                throw new AuthenticationError('no user found with that email address');
+            }
+
+            // if there is a user found, execute the isCorrectPassword  instance method and check i the correct pass word was provided. 
+            const correctPw = await user.isCorrectPassword(password);
+
+            // if the passwrod is wrong
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            // if email n password are right , sign user into the app with JWT
+            const token = signToken(user);
+
+            // return an 'auth' object that consist of the signed token and user info
+            return { token, user };
+
+        }
     }
-};
+}
 
 module.exports = resolvers;
