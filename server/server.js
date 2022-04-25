03cfc373b7b952ba606cb2cad69b548d9809c4a9
ElmerPC <erey2790@gmail.com>
2022-04-25 04:27:17 -0500

    heroku created

--- server/server.js
+++ server/server.js
@@ -40,12 +40,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
 app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+
+// fathomless-sierra-24178
+
+// https://fathomless-sierra-24178.herokuapp.com/
+
+// https://git.heroku.com/fathomless-sierra-24178.git
